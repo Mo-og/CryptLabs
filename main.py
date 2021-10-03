27e61b839d30db6429632012c06fb641744e95a6
Maks <mgorbiik@mail.ru>
2021-10-03 18:28:26 +0300

    Lab 3 finished

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import random
 from itertools import cycle
 
@@ -13,38 +14,48 @@ def decrypt(text: str, gamma):
     return result
 
 
-def get_byte(c_num: int = -1, char: str = None):
-    if c_num == -1:
-        c_num = ord(char)
+def get_byte(char: str = None, num: int = -1):
+    if num == -1:
+        num = ord(char)
+    # print("ch=", char, "num=", num)
     a = [0, 0, 0, 0, 0, 0, 0, 0]
-    c = a
-    if c_num > 255:
-        for i in range(c_num // 255 - 1):
+    c = [0, 0, 0, 0, 0, 0, 0, 0]
+    log = math.log2(num)
+    log8 = int(log // 8)
+    if log8 > 0:
+        for i in range(log8 - 1):
             c += a
-        if c_num % 255 > 0:
+        if log % 8 != 0:
             c += a
-
-    ch_bin = bin(c_num)[2:]
+    ch_bin = bin(num)[2:]
     for i in range(len(ch_bin)):
-        c[len(c) - 1 - i] = int(ch_bin[i])
+        c[len(c) - 1 - i] = int(ch_bin[len(ch_bin) - 1 - i])
     c_str = ''
     for n in c:
         c_str += str(n)
     return c_str
 
 
-def crypt(text: str, key: str):
+def crypt(text: str, key: str='', debug=False, decrypt=False, gamma=None):
     result = ''
-    random.seed(key)
-    rand = []
-    lets = []
-    for x in text:
-        if not x:
-            break
-        r = random.randint(0, MAX_NUM)
-        rand.append(r)
-        lets.append(ord(x))
-        result += chr((ord(x) ^ r) % CHARS_IN_UTF)  # всё к-во символов в таблице кодировки
+    rands = []
+    if gamma is not None:
+        rands = gamma
+    else:
+        random.seed(key)
+        for i in range(len(text)):
+            rands.append(random.randint(0, MAX_NUM))
+        # if decrypt: rands.reverse()
+    for x in range(len(text)):
+        r = rands[x]
+        char = get_byte(char=text[x])
+        debug_ch = "ch = " + char + " // = "
+        char = int(char, 2)
+        debug_ch += str(char) + " = " + text[x]
+        debug_rand = "rd = " + get_byte(num=r) + " // = " + str(r)
+        result += chr(char ^ r)  # всё к-во символов в таблице кодировки
+        debug_res = "rs = " + get_byte(chr(char ^ r)) + " // = " + str(char ^ r)
+        if debug: print(debug_ch + '\n' + debug_rand + '\n' + debug_res)
     return result
 
 
@@ -56,15 +67,15 @@ def get_letters_codes(text):
 
 
 key = 'whatever text is here is a key 7128%*$^#'
-phrase = 'Текстовая фраза'
+phrase = 'hello world'
 
 print("Исходный текст: '" + phrase + "'")
-u = crypt(phrase, key)
+u = crypt(phrase, key, True)
 print("Зашифрованый текст: '" + str(u) + "'")
-print("Расшифрованый текст: '" + str(crypt(u, key)) + "'")
+print("Расшифрованый текст: '" + str(crypt(u, key, True)) + "'")
 
 a = get_letters_codes(phrase)  # коды чистого текста
-b = get_letters_codes(crypt('Чеснок это овощ', key))  # коды зашифрованого текста (желаемый левый вариант)
+b = get_letters_codes(crypt('unknown tax', key))  # коды зашифрованого текста (желаемый левый вариант)
 c = []
 for (x, y) in zip(a, cycle(b)):
     c.append(x ^ y)  # операция xor этих кодов даст гамму, которой нужно гамировать зашифрованный чистый текст,
@@ -73,5 +84,5 @@ s = ''
 for i in c:
     s += "%d " % i
 print('Пример гаммы для некорректной расшифровки:\n' + s[:-2])
-s = decrypt(crypt(phrase, key), c)
+s = crypt(crypt(phrase, key), gamma=c)
 print("Фраза, расшифрованная этой гаммой:'" + s + "'")
